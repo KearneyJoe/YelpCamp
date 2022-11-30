75b3b38f2c8d1746565f0606da97c7ffe409111c
Joe Kearney <joekearney@gennas-mbp.myfiosgateway.com>
2022-11-30 15:09:57 -0500

    Add start script

--- app.js
+++ app.js
@@ -111,7 +111,8 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log("On port 3000!")
+app.listen(port, () => {
+    console.log(`On port ${port}!`)
 })
